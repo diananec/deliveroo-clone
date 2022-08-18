e27c10abe374615ea95d6ed9dec440e6b82e628b
Diana Nechita <diananechita1999@gmail.com>
2022-08-18 16:21:37 +0300

    Working. Needs more features

---
+++ deliveroo-clone/components/FeaturedRow.js
@@ -0,0 +1,43 @@
+import { View, Text, ScrollView } from "react-native";
+import React, { useEffect } from "react";
+import { ArrowRightIcon } from "react-native-heroicons/outline";
+import RestaurantCard from "./RestaurantCard";
+import sanityClient from "../sanity";
+
+const FeaturedRow = ({ id, title, description }) => {
+  useEffect(() => {
+    //sanityClient.fetch(``)
+  }, []);
+
+  return (
+    <View>
+      <View className="mt-4 flex-row items-center justify-between px-4">
+        <Text className="font-bold text-lg">{title}</Text>
+        <ArrowRightIcon color="#00BBCC" />
+      </View>
+      <Text className="text-xs text-gray-500 px-4">{description}</Text>
+      <ScrollView
+        horizontal
+        contentContainerStyle={{ paddingHorizontal: 15 }}
+        showsHorizontalScrollIndicator={false}
+        className="pt-4"
+      >
+        {/*Restaurant Cards*/}
+        <RestaurantCard
+          id={123}
+          imgUrl="https://links.papareact.com/gn7"
+          title="Yo!Sushi"
+          rating={4.5}
+          genre="Japanese"
+          address="123 Main St"
+          short_description="This is Sushi description"
+          dishes={[]}
+          long={20}
+          lat={0}
+        />
+      </ScrollView>
+    </View>
+  );
+};
+
+export default FeaturedRow;
